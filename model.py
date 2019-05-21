f514fad5219e8ce724f669d946c6222e86ae6957
a4assassin <43116982+a4assassin@users.noreply.github.com>
2019-05-21 18:50:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+
 import PIL.Image
 import cv2
 import torch
@@ -6,9 +7,61 @@ import pickle
 import matplotlib.pyplot as plt
 torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
-layer_dims = [1600, 50, 2]
 
-font=cv2.FONT_HERSHEY_SIMPLEX
+def init_parameters(layer_dims):
+    
+    L = len(layer_dims)
+    parameters = {}
+    print("Initializing parameters....")
+
+    for l in range(1, L):
+        parameters["W" + str(l)] = torch.cuda.FloatTensor(layer_dims[l], layer_dims[l-1]).normal_() * 0.01
+        parameters["b" + str(l)] = torch.cuda.FloatTensor(layer_dims[l],1).normal_() * 0
+        
+        assert(parameters["W" + str(l)].shape == torch.Size([layer_dims[l],layer_dims[l-1]]))
+        assert(parameters["b" + str(l)].shape == torch.Size([layer_dims[l],1]))
+        
+    print("Parameters Initialized....")
+
+    for i in range(1, L):
+        print("Shape of W{} : ".format(i), parameters["W" + str(i)].shape, "\t", parameters["W" + str(i)].type())
+    
+    return parameters
+
+
+
+def init_training_set():
+    
+    print("Initializing Training Set...") 
+    with open("train_X.pickle", "rb") as file:
+        train_X = pickle.load(file)
+        
+    with open("train_Y.pickle", "rb") as file:
+        train_Y = pickle.load(file)
+        
+    with open("test_X.pickle", "rb") as file:
+        test_X = pickle.load(file)
+        
+    with open("test_Y.pickle", "rb") as file:
+        test_Y = pickle.load(file)
+    
+    train_X = train_X/255
+    test_X = test_X/255
+        
+    train_Y = np.eye(2)[train_Y].T
+    train_Y = np.reshape(train_Y, (train_Y.shape[0], train_Y.shape[1] * train_Y.shape[2]))
+    test_Y = np.eye(2)[test_Y].T
+    test_Y = np.reshape(test_Y, (test_Y.shape[0], test_Y.shape[1] * test_Y.shape[2]))
+    print("Training Set initialized...") 
+    
+    train_X = torch.from_numpy(train_X).type(torch.cuda.FloatTensor)
+    train_Y = torch.from_numpy(train_Y).type(torch.cuda.FloatTensor)
+    test_X = torch.from_numpy(test_X).type(torch.cuda.FloatTensor)
+    test_Y = torch.from_numpy(test_Y).type(torch.cuda.FloatTensor)
+    
+    return train_X, train_Y, test_X, test_Y
+
+
 
 def non_linear_activation(Z, activation = "ReLU"):
     if activation == "tanh" :
@@ -18,10 +71,9 @@ def non_linear_activation(Z, activation = "ReLU"):
         zero = torch.cuda.FloatTensor([0])
         Z = torch.max(zero.expand_as(Z), Z)
         
-    if activation == "softmax":
-        Z = torch.exp(Z)
-        Z_sum = torch.sum(Z, dim=0)
-        Z = Z/Z_sum
+    if activation == "sigmoid":
+        Z = torch.sigmoid(Z)
+        
     
     return Z
 
@@ -34,66 +86,124 @@ def forward_propagation(X, parameters):
     
     for l in range(1, L):
         cache["Z" + str(l)] = torch.mm(parameters["W" + str(l)], A) + parameters["b" + str(l)]
-        cache["A" + str(l)] = non_linear_activation(cache["Z" + str(l)], activation = "tanh")
+        cache["A" + str(l)] = non_linear_activation(cache["Z" + str(l)], activation = "ReLU")
 
         assert(cache["Z" + str(l)].shape == cache["A" + str(l)].shape)
         A = cache["A" + str(l)]
         
-    cache["A" + str(L - 1)] = non_linear_activation(cache["Z" + str(L - 1)], activation = "softmax")
+    cache["A" + str(L - 1)] = non_linear_activation(cache["Z" + str(L - 1)], activation = "sigmoid")
     assert(cache["Z" + str(L - 1)].shape == cache["A" + str(L - 1)].shape)
 
     return cache
 
 
-def detect_face(test_X, layer_dims):
+def compute_cost(Y, AL):
     
-    while True:
-        indx = int(input("Enter the numeber < 2000 : "))
-        if indx == -1:
-          break
-        X = test_X[:, indx].view(1600, 1)
-        print(X.shape)
-        img = X.contiguous().cpu().numpy().reshape(40, 40)
+    m = int(Y.shape[1])
+    cost = (-1/m) * torch.sum(Y * torch.log(AL) + (1 - Y) * torch.log(1 - AL))
+    
+    return cost
+
+
+def relu_derivative(Z):
+    Z[Z <= 0] = 0
+    Z[Z > 0] = 1
+    return Z
+
+def backpropagation(Y, cache, parameters):
+    
+    grads = {}
+    L = len(layer_dims) - 1
+    
+    m = int(Y.shape[1])
+    
+    grads["dZ{}".format(L)] = (cache["A{}".format(L)] - Y) 
+    grads["dW{}".format(L)] = (1/m) * torch.mm(grads["dZ{}".format(L)], cache["A{}".format(L - 1)].permute(1,0))
+    grads["db{}".format(L)] = (1/m) * torch.sum(grads["dZ{}".format(L)], dim=1, keepdim=True)
+    
+    W = parameters["W{}".format(L)]
+    dZ = grads["dZ{}".format(L)]
+    for l in range(1, L):
+        
+        grads["dA" + str(L - l)] = torch.mm(W.permute(1,0), dZ) 
+        grads["dZ" + str(L - l)] = grads["dA" + str(L - l)] * relu_derivative(cache["Z" + str(L - l)])
+        grads["dW" + str(L - l)] = ((1/m) * torch.mm(grads["dZ" + str(L - l)], cache["A" + str(L - l - 1)].permute(1,0)))
+        grads["db" + str(L - l)] = (1/m) * torch.sum(grads["dZ" + str(L - l)], dim=1, keepdim=True)
+        
+        assert(grads["dA" + str(L - l)].shape == cache["A" + str(L - l)].shape)
+        assert(grads["dZ" + str(L - l)].shape == cache["Z" + str(L - l)].shape)
+        assert(grads["dW" + str(L - l)].shape == parameters["W" + str(L - l)].shape)
+        assert(grads["db" + str(L - l)].shape == parameters["b" + str(L - l)].shape)
+        
+        W = parameters["W" + str(L - l)]
+        dZ = grads["dZ" + str(L - l)]
+    
+    return grads
+
+
+def update_parameters(parameters, grads, layer_dims, learning_rate = 0.03):
+    
+    L = len(layer_dims)
+    
+    for l in range(1,L):
+        
+        parameters["W" + str(l)] = parameters["W" + str(l)] - (learning_rate * grads["dW" + str(l)])
+        parameters["b" + str(l)] = parameters["b" + str(l)] - (learning_rate * grads["db" + str(l)])
+    
+    return parameters
+
+
+
+def model(X, Y, layer_dims, num_of_iterations, learning_rate = 0.05):
+    
+    costs = []
+    cost = 0
+    L = len(layer_dims)
+    m = int(Y.shape[1])
+    parameters = init_parameters(layer_dims)
+    train_accuracy = 0
+    
+    for i in range(num_of_iterations):
         cache = forward_propagation(X, parameters)
-        out = list(cache["A2"].cpu().numpy().squeeze())
-        maximum = out.index(max(out))
-
-        plt.title("{}".format(classes[str(maximum)]))
-        plt.imshow(img, cmap="gray")
-        plt.show()
-
-        print("Probability distribution : ", out)
-
-def real_time_detection():
-  
-    parameters = pickle.load(open("trained_parameters_65_69.pickle", "rb"))
-    classes = {"0":"Female", "1":"Male"}
+        AL = cache["A{}".format(L-1)]
+        cost = compute_cost(Y, AL)
+        grads = backpropagation(Y, cache, parameters)
+        parameters = update_parameters(parameters, grads, layer_dims, learning_rate)
+        costs.append(cost)
+        
+        if i%1000 == 0:
+            print("Cost after ", i, " iterations : ", cost)
     
-    detector = cv2.CascadeClassifier('E:\ML project\haarcascade_frontalface_default.xml')
-    cam=cv2.VideoCapture(0)
-
-    while(True):
-        ret,img=cam.read();
-        grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        faces=detector.detectMultiScale(grey);
-        for(x,y,w,b) in faces:
-            cv2.rectangle(img,(x-20,y-40),(x+w+20,y+b+20),(255,0,0),2)
-            new = cv2.resize(grey[y:y+b,x:x+w], (40, 40)) / 255
-            X = torch.from_numpy(np.reshape(new, (1600, 1))).type(torch.cuda.FloatTensor)
-            cache = forward_propagation(X, parameters)
-            out = list(cache["A2"].cpu().numpy().squeeze())
-            maximum = out.index(max(out))
-            out1=float("{0:.1f}".format(max(out)*100))
-            cv2.putText(img,(classes[str(maximum)])+"  "+str(out1)+"%",(x,y+b+45),font,1,(0,255,0),2)
-            
-        cv2.imshow("Camera",img);
-        if(cv2.waitKey(1)==ord('q')):
-            break;
-    cam.release()
-    cv2.destroyAllWindows()    
-    exit(0)
+    cache = forward_propagation(X, parameters)
+    AL = cache["A" + str(L - 1)]
+    AL[AL <= 0.5] = 0
+    AL[AL > 0.5] = 1
+    diff = (torch.sum(torch.abs(train_Y - AL))/(m)) * 100
+    train_accuracy = 100 - diff
+    
+    plt.plot(costs)
+    plt.xlabel("Iterations")
+    plt.ylabel("Cost")
+    plt.show()
+    
+    return parameters, train_accuracy
 
 
-real_time_detection()
 
 
+
+a = torch.cuda.FloatTensor([3.12])
+print(a.cpu().numpy().squeeze())
+
+layer_dims = [1600, 50, 2, 1]
+print('Initialising training set')
+train_X, train_Y, test_X, test_Y = init_training_set()
+optimized_parameters, train_accuracy = model(train_X, train_Y, layer_dims, num_of_iteratiotns =600000, learning_rate = 0.001)
+
+print("\n\nWriting parameters to disk...")
+optimized_params = open("trained_parameters", "wb")
+pickle.dump(optimized_parameters, optimized_params)
+optimized_params.close()
+print("Parameters written to disk...")
+
+print("\n\n Trained Accuracy : ", train_accuracy)
